b6b3bbd08b298cafecb833ab8308b71a3d55c156
Biaoshen98 <c1006558@newcastle.ac.uk>
2022-08-21 20:35:19 +0100

    Add question

--- 3D rome/Assets/Scripts/Answer.cs
+++ 3D rome/Assets/Scripts/Answer.cs
@@ -4,30 +4,30 @@ using UnityEngine.UI;
 
 public class Answer : MonoBehaviour
 {
-    //读取文档
-    string[][] ArrayX;//题目数据
-    string[] lineArray;//读取到题目数据
-    private int topicMax = 0;//最大题数
-    private List<bool> isAnserList = new List<bool>();//存放是否答过题的状态
+    //read txt
+    string[][] ArrayX;
+    string[] lineArray;
+    private int topicMax = 0;
+    private List<bool> isAnserList = new List<bool>();
 
-    //加载题目
-    public GameObject tipsbtn;//提示按钮
-    public Text tipsText;//提示信息
-    public List<Toggle> toggleList;//答题Toggle
-    public Text indexText;//当前第几题
-    public Text TM_Text;//当前题目
-    public List<Text> DA_TextList;//选项
-    private int topicIndex = 0;//第几题
+    //load question
+    public GameObject tipsbtn;
+    public Text tipsText;
+    public List<Toggle> toggleList;
+    public Text indexText;
+    public Text TM_Text;
+    public List<Text> DA_TextList;
+    private int topicIndex = 0;
 
-    //按钮功能及提示信息
-    public Button BtnBack;//上一题
-    public Button BtnNext;//下一题
-    public Button BtnTip;//消息提醒
-    //public Button BtnJump;//跳转题目
-    //public InputField jumpInput;//跳转题目
-    public Text TextAccuracy;//正确率
-    private int anserint = 0;//已经答过几题
-    private int isRightNum = 0;//正确题数
+    //Function of BUtton
+    public Button BtnBack;
+    public Button BtnNext;
+    public Button BtnTip;
+    //public Button BtnJump;
+    //public InputField jumpInput;
+    public Text TextAccuracy;
+    private int anserint = 0;
+    private int isRightNum = 0;
 
     void Awake()
     {
@@ -49,21 +49,21 @@ public class Answer : MonoBehaviour
     }
 
 
-    /*****************读取txt数据******************/
+    /*****************load txt******************/
     void TextCsv()
     {
-        //读取csv二进制文件  
+        
         TextAsset binAsset = Resources.Load("YW", typeof(TextAsset)) as TextAsset;
-        //读取每一行的内容  
+        
         lineArray = binAsset.text.Split('\r');
-        //创建二维数组  
+         
         ArrayX = new string[lineArray.Length][];
-        //把csv中的数据储存在二维数组中  
+         
         for (int i = 0; i < lineArray.Length; i++)
         {
             ArrayX[i] = lineArray[i].Split(':');
         }
-        //设置题目状态
+        
         topicMax = lineArray.Length;
         for (int x = 0; x < topicMax + 1; x++)
         {
@@ -71,7 +71,7 @@ public class Answer : MonoBehaviour
         }
     }
 
-    /*****************加载题目******************/
+    /*****************load question******************/
     void LoadAnswer()
     {
         for (int i = 0; i < toggleList.Count; i++)
@@ -86,21 +86,21 @@ public class Answer : MonoBehaviour
         tipsbtn.SetActive(false);
         tipsText.text = "";
 
-        indexText.text = "第" + (topicIndex + 1) + "题：";//第几题
-        TM_Text.text = ArrayX[topicIndex][1];//题目
-        int idx = ArrayX[topicIndex].Length - 3;//有几个选项
+        indexText.text = "The " + (topicIndex + 1) + " Question: ";
+        TM_Text.text = ArrayX[topicIndex][1];
+        int idx = ArrayX[topicIndex].Length - 3;
         for (int x = 0; x < idx; x++)
         {
-            DA_TextList[x].text = ArrayX[topicIndex][x + 2];//选项
+            DA_TextList[x].text = ArrayX[topicIndex][x + 2];
         }
     }
 
-    /*****************按钮功能******************/
+    /*****************Button******************/
     void Select_Answer(int index)
     {
         switch (index)
         {
-            case 0://提示
+            case 0:
                 int idx = ArrayX[topicIndex].Length - 1;
                 int n = int.Parse(ArrayX[topicIndex][idx]);
                 string nM = "";
@@ -119,9 +119,9 @@ public class Answer : MonoBehaviour
                         nM = "D";
                         break;
                 }
-                tipsText.text = "<color=#FFAB08FF>" +"正确答案是："+ nM + "</color>";
+                tipsText.text = "<color=#FFAB08FF>" +"The right answer is："+ nM + "</color>";
                 break;
-            case 1://上一题
+            case 1:
                 if (topicIndex > 0)
                 {
                     topicIndex--;
@@ -129,10 +129,10 @@ public class Answer : MonoBehaviour
                 }
                 else
                 {
-                    tipsText.text = "<color=#27FF02FF>" + "前面已经没有题目了！" + "</color>";
+                    tipsText.text = "<color=#27FF02FF>" + "None before this question！" + "</color>";
                 }
                 break;
-            case 2://下一题
+            case 2:
                 if (topicIndex < topicMax-1)
                 {
                     topicIndex++;
@@ -140,7 +140,7 @@ public class Answer : MonoBehaviour
                 }
                 else
                 {
-                    tipsText.text = "<color=#27FF02FF>" + "哎呀！已经是最后一题了。" + "</color>";
+                    tipsText.text = "<color=#27FF02FF>" + "None after this question." + "</color>";
                 }
                 break;
             // case 3://跳转
@@ -164,19 +164,18 @@ public class Answer : MonoBehaviour
     {
         if (check)
         {
-            //判断题目对错
             bool isRight;
             int idx = ArrayX[topicIndex].Length - 1;
             int n = int.Parse(ArrayX[topicIndex][idx]) - 1;
             if (n == index)
             {
-                tipsText.text = "<color=#27FF02FF>" + "恭喜你，答对了！" + "</color>";
+                tipsText.text = "<color=#27FF02FF>" + "Congratulations! " + "</color>";
                 isRight = true;
                 tipsbtn.SetActive(true);
             }
             else
             {
-                tipsText.text = "<color=#FF0020FF>" + "对不起，答错了！" + "</color>";
+                tipsText.text = "<color=#FF0020FF>" + "You are wroing！" + "</color>";
                 isRight = false;
                 tipsbtn.SetActive(true);
             }
@@ -184,7 +183,7 @@ public class Answer : MonoBehaviour
             //正确率计算
             if (isAnserList[topicIndex])
             {
-                tipsText.text = "<color=#FF0020FF>" + "这道题已答过！" + "</color>";
+                tipsText.text = "<color=#FF0020FF>" + "You have answered this question." + "</color>";
             }
             else
             {
@@ -194,7 +193,7 @@ public class Answer : MonoBehaviour
                     isRightNum++;
                 }
                 isAnserList[topicIndex] = true;
-                TextAccuracy.text = "正确率：" + ((float)isRightNum / anserint * 100).ToString("f2") + "%";
+                TextAccuracy.text = "The percentage of correct:" + ((float)isRightNum / anserint * 100).ToString("f2") + "%";
             }
 
             //禁用掉选项
