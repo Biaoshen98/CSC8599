a4f3eb56b911015f8188bfe3bb39d3b6e32412fc
Biaoshen98 <c1006558@newcastle.ac.uk>
2022-09-01 14:55:27 +0100

    oh

--- 3D rome/Assets/Scripts/Lord.cs
+++ 3D rome/Assets/Scripts/Lord.cs
@@ -24,7 +24,7 @@ public class Lord : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.V)&&playerNpc == true){
             dialogueBox.SetActive(true);
-            textBox1.text = "I am the lord of this city!\nI allow you to move around in the city!\nYou can challenge my warriors if you want!";
+            textBox1.text = "My name is Joseph Flavius Cerialis.\nI am the lord of this city!\nI allow you to move around in the city!\nYou can challenge my warriors if you want!";
             textBox4.text = " ";
         }
         if(playerNpc != true){
